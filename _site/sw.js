5f11425fc86d3cf131b6d554919c29ede0b79217
infominer <infominer@protonmail.com>
2019-06-28 22:31:00 -0400

    add bootstrap

--- _site/sw.js
+++ _site/sw.js
@@ -1,5 +1,5 @@
-const cacheName = 'sw-1561738417';
-const dataCacheName = 'sw-1561738417';
+const cacheName = 'sw-1561775434';
+const dataCacheName = 'sw-1561775434';
 const RUNTIME = 'runtime';
 
 var getPageURL = location;
